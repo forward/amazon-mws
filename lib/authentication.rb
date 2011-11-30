cba78b309081e05c03e81854793befe73d62d28a
Christian Blunden <christian.blunden@gmail.com>
2011-11-30 12:04:36 +0000

    increased version to 2011-01-01

--- lib/authentication.rb
+++ lib/authentication.rb
@@ -1,6 +1,6 @@
 module AmazonMWS
   class Authentication  
-    VERSION = '2009-01-01'
+    VERSION = '2011-01-01'
     # Nothing really to see here, 
     # please have a look at the files in the authentication folder
   end
